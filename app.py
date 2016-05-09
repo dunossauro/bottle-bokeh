f270fc60951cbc9ac7718b3c701720ba56761956
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2016-05-09 16:30:13 -0300

    Lines and Bars

--- app.py
+++ app.py
@@ -1,21 +1,40 @@
 from bokeh.plotting import figure
+from bokeh.charts import Bar
 from bokeh.embed import file_html
 from bokeh.resources import CDN
 from bottle import Bottle, jinja2_view
+
+
 app = Bottle()
 
+# =========================== Start Bar
+data = {
+    'Palavras': [x for x in range(21)],
+    'frequência': [x for x in range(21)]
+}
 
+bar = Bar(data, values='frequência', label='Palavras',
+          title="Teste")
+
+graph_bar = file_html(bar, CDN)
+
+# =========================== End Bar
+
+# =========================== Start Line
 x = [1, 9, 3, 4, 5]
 y = [6, 7, 2, 4, 5]
 
 p = figure(title="Exemplo Simples", x_axis_label='x', y_axis_label='y')
 p.line(x, y, legend="Linha Azul", line_width=2)
 
-graph = file_html(p, CDN)
+graph_line = file_html(p, CDN)
+# =========================== End Line
 
 @app.route("/")
 @jinja2_view('saida.html')
 def grap():
-    return dict(bokeh=graph, texto="Um exemplo simples integrando o Bokeh, Jinja2 e Bottle")
+    return dict(bokeh_line=graph_line,
+                bokeh_bar=graph_bar,
+                texto="Um exemplo simples integrando o Bokeh, Jinja2 e Bottle")
 
 app.run()
